7d266c370385741b9199aff1fabe09dee87ec903
Jakub Wrabel <jakub.wrabel@ocado.com>
2017-05-10 20:43:45 +0200

    sdsd

--- src/main/java/pl/jwrabel/trainings/javandwro3/BufferedStreams.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/BufferedStreams.java
@@ -1,10 +1,39 @@
 package pl.jwrabel.trainings.javandwro3;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class BufferedStreams {
 
     public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("a.txt");
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int read;
+        while ((read = bufferedInputStream.read()) > 0) {
+            stringBuilder.append((char) read);
+        }
+
+        System.out.println(stringBuilder.toString());
+
+        bufferedInputStream.close();
+        fileInputStream.close();
+
+        FileOutputStream fileOutputStream = new FileOutputStream("b.txt");
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+
+        bufferedOutputStream.write('A');
+        bufferedOutputStream.write('B');
+        bufferedOutputStream.write('C');
+        bufferedOutputStream.write('D');
+
+        bufferedOutputStream.flush();
+        bufferedOutputStream.close();
+        fileOutputStream.close();
 
     }
 
